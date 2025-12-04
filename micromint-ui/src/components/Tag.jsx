36b20d42355a914e021e4fe93bfd68a040e805a0
Aadya1504 <aadyasr1504@gmail.com>
2025-12-04 19:35:47 +0530

    Delete micromint-ui directory

--- micromint-ui/src/components/Tag.jsx
+++
@@ -1,8 +0,0 @@
-export function Tag({ label }) {
-    return (
-      <span className="px-3 py-1 rounded-full bg-gray-100 text-xs text-gray-600">
-        {label}
-      </span>
-    );
-  }
-  
--- micromint-ui/src/components/TopicTag.jsx
+++
@@ -1,8 +0,0 @@
-export default function TopicTag({ text }) {
-    return (
-      <span className="px-3 py-1 bg-gray-100 text-gray-700 text-sm rounded-full">
-        {text}
-      </span>
-    );
-  }
-  
